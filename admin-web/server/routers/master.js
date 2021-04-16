43d996cbb0e9c4253fbec678c88c7198ec3645b8
EvanZhao98 <1823931512@qq.com>
2021-04-16 23:50:44 +0800

    fix: 删除一些不必要文件

--- admin-web/server/routers/master.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get('/master', (req, res) => {
-    res.send('hello master!')
-})
-
-module.exports = router
--- admin-web/server/routers/user.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get('/user', (req, res) => {
-    res.send('hello user!')
-})
-
-module.exports = router
